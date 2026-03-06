02419a259f7cb89fc589ccb91dcababde8e9a14f
mohan1389 <146318390+mohan1389@users.noreply.github.com>
2026-03-06 15:01:50 +0530

    Update House Robber.cpp

--- House Robber.cpp
+++ House Robber.cpp
@@ -29,3 +29,41 @@ public:
         return max(pick,notPick);
     }
 };
+
+// MEMOIZATION
+//TC: O(n) + O(n)
+//SC: O(n) + O(n) + O(n) - auxiliary stack space + 2 dp array size
+class Solution {
+public:
+    int houseRobber(vector<int>& money) {
+        int n = money.size();
+        int ind = n-1;
+        vector<int>dp1(n,-1);
+        vector<int>dp2(n,-1);
+        return max(f1(money,ind,dp1),f2(money,ind-1,dp2));
+    }
+
+    int f1(vector<int>&arr, int ind, vector<int>&dp1){
+        if(ind == 1) return arr[1];
+        if(ind < 1) return 0;
+
+        if(dp1[ind] != -1) return dp1[ind];
+
+        int pick = arr[ind] + f1(arr,ind-2,dp1);
+        int notPick = 0 + f1(arr,ind-1,dp1);
+
+        return dp1[ind] = max(pick,notPick);
+    }
+
+    int f2(vector<int>&arr, int ind, vector<int>&dp2){
+        if(ind == 0) return arr[0];
+        if(ind < 0) return 0;
+
+        if(dp2[ind] != -1) return dp2[ind];
+
+        int pick = arr[ind] + f2(arr,ind-2,dp2);
+        int notPick = 0 + f2(arr,ind-1,dp2);
+
+        return dp2[ind] = max(pick,notPick);
+    }
+};
