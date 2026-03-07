cc7b93a6bf3654d7cafbde3af1207dada042a234
mohan1389 <146318390+mohan1389@users.noreply.github.com>
2026-03-07 20:48:20 +0530

    Update 63. Unique Paths II.cpp

--- 63. Unique Paths II.cpp
+++ 63. Unique Paths II.cpp
@@ -81,3 +81,35 @@ public:
         return dp[m-1][n-1];
     }
 };
+
+
+//SPACE-OPTIMIZATION
+//TC: O(n*m)
+//SC: O(m) - curr and prevarray size
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int m = obstacleGrid.size();
+        int n = obstacleGrid[0].size();
+        vector<int> prev(n,-1);
+
+        for(int i=0; i<m; i++){
+            vector<int> curr(n, 0);
+            for(int j=0; j<n; j++){
+                if(i >= 0 && j >= 0 && obstacleGrid[i][j] == 1) curr[j] = 0;
+                else if(i == 0 && j == 0) curr[j] = 1;
+                else{
+                   int top = 0, left = 0;
+
+                    if(i>0) top = top + prev[j];
+                    if(j>0) left = left + curr[j-1];
+
+                    curr[j] = top + left;
+
+                }
+            }
+            prev = curr;
+        }
+        return prev[n-1];
+    }
+};
