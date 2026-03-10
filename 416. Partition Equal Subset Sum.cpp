c3082fd5af9acfc0b01926b550c26af61909e064
mohan1389 <146318390+mohan1389@users.noreply.github.com>
2026-03-11 03:40:19 +0530

    Update 416. Partition Equal Subset Sum.cpp

--- 416. Partition Equal Subset Sum.cpp
+++ 416. Partition Equal Subset Sum.cpp
@@ -54,3 +54,38 @@ public:
         return dp[ind][target] = pick || notPick;
     }
 };
+
+
+
+class Solution {
+public:
+    bool canPartition(vector<int>& nums) {
+       int sum = 0;
+        int n = nums.size();
+
+        for(int i=0; i<n; i++){
+            sum = sum + nums[i];
+        }
+        if(sum & 1) return false;
+
+        int target = sum / 2;
+        vector<vector<bool>> dp(n, vector<bool>(target+1, false));
+
+        for(int i = 0; i < n; i++)
+            dp[i][0] = true;
+
+        if(nums[0] <= target)
+            dp[0][nums[0]] = true;
+
+        for(int ind = 1; ind < n; ind++){
+            for(int k = 1; k <= target; k++){
+                bool notPick = dp[ind-1][k];
+                bool pick = false;
+                if(k >= nums[ind])
+                    pick = dp[ind-1][k-nums[ind]];
+                dp[ind][k] = pick || notPick;
+            }
+        }
+        return dp[n-1][target];
+    }
+};
