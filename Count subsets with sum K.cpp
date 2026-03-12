1327742d83559e1c976c1aefcbca7a9e42507340
mohan1389 <146318390+mohan1389@users.noreply.github.com>
2026-03-12 13:20:43 +0530

    Update Count subsets with sum K.cpp

--- Count subsets with sum K.cpp
+++ Count subsets with sum K.cpp
@@ -54,3 +54,33 @@ class Solution{
         return dp[ind][target] = notPick + pick;
     }
 };
+
+//TABULATION
+//TC: O(n*K)
+//SC: O(n*K) - dp array size
+class Solution{
+	public:
+	int perfectSum(vector<int>&arr, int K){
+        int n = arr.size();
+        vector<vector<int>> dp(n, vector<int>(K+1, 0));
+        for(int i=0; i<n; i++){
+            dp[i][0] = 1;
+        }
+        if(arr[0] == 0) dp[0][0] = 2;
+        
+        if(arr[0] != 0 && arr[0] <= K)
+            dp[0][arr[0]] = 1;
+
+        for(int ind=1; ind<n; ind++){
+            for(int target = 0; target<= K; target++){
+                int notPick = dp[ind-1][target];
+                int pick = 0;
+                if(arr[ind] <= target) pick = dp[ind-1][target-arr[ind]];
+
+                dp[ind][target] = notPick + pick;
+            }
+        }
+
+        return dp[n-1][K];
+	}
+};
