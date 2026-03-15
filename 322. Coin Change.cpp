756f104ae1dc38f514302f72c01a8d6166249849
mohan1389 <146318390+mohan1389@users.noreply.github.com>
2026-03-16 03:51:28 +0530

    Refactor coinChange with memoization and update comments
    Updated space complexity comment and added memoization implementation for coin change problem.

--- 322. Coin Change.cpp
+++ 322. Coin Change.cpp
@@ -1,6 +1,6 @@
 //RECURRSIVE
 //TC: O(2^n)
-//SC: O(n) <-- isse thodi jada hogi complexity
+//SC: O(amount) <-- isse thodi jada hogi complexity
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
@@ -25,5 +25,39 @@ public:
         }
 
         return min(pick, notPick);
+    }
+};
+
+
+//MEMOIZATION
+//TC: O(n * amount)
+//SC: O(n * amount) + O(amount)
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        int n = coins.size();
+        vector<vector<int>> dp(n, vector<int>(amount+1, -1));
+        int ans = f(n-1, coins, amount, dp);
+        if(ans >= 1e9) return -1;
+        return ans;
+    }
+
+    int f(int ind, vector<int>& coins, int amount, vector<vector<int>> &dp){
+        if(amount == 0) return 0; // no more coins required
+        if(ind == 0){
+            if(amount % coins[ind] == 0) return amount / coins[ind];
+            else return 1e9;
+        }
+
+        if(dp[ind][amount] != -1) return dp[ind][amount];
+
+        int notPick = 0 + f(ind-1, coins, amount, dp);
+
+        int pick = 1e9;
+        if(coins[ind] <= amount){
+            pick = 1 + f(ind, coins, amount-coins[ind], dp);
+        }
+
+        return dp[ind][amount] = min(pick, notPick);
     }
 };
