0ea7136f3aae4746111c2856a79a9ad35984b1ec
mohan1389 <146318390+mohan1389@users.noreply.github.com>
2026-03-28 04:26:34 +0530

    Implement longest palindromic subsequence algorithm

---
+++ 516. Longest Palindromic Subsequence.cpp
@@ -0,0 +1,17 @@
+//Approach: reverse the given string and find the LCS of two strings
+class Solution {
+public:
+    int longestPalindromeSubseq(string s) {
+        string s1 = s;
+        string s2 = s;
+        reverse(s2.begin(), s2.end());
+
+        return f(s1.size(), s2.size(), s1, s2);
+    }
+
+    int f(int i, int j, string &s1, string &s2){
+        if(i==0 || j==0) return 0;
+        if(s1[i-1] == s2[j-1]) return 1 + f(i-1,j-1, s1, s2);
+        return max(f(i-1,j, s1, s2), f(i,j-1, s1, s2));
+    }
+};
