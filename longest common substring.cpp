a7b012de907a0eb3b6bd45a2c55fc0b7ba5fd7e4
mohan1389 <146318390+mohan1389@users.noreply.github.com>
2026-03-27 22:12:41 +0530

    Implement longest common substring function

---
+++ longest common substring.cpp
@@ -0,0 +1,24 @@
+//BEST APPROACH: SAME CODE OF LCS TABULATION, JUST RETURN THE MAXIMUM VALUE OF THE DP ARRAY
+class Solution{
+    public: int longestCommonSubstr (string str1, string str2){
+        int n = str1.size();
+        int m = str2.size();
+        int ans = 0;
+
+        // dp[i][j] = LCS of first i chars of str1 and first j chars of str2
+        vector<vector<int>> dp(n+1 , vector<int>(m+1, 0));
+
+        for (int ind1 = 1; ind1 <=n; ind1++) {
+            for (int ind2 = 1; ind2 <=m; ind2++) {
+                if (str1[ind1-1] == str2[ind2-1]) {
+                    dp[ind1][ind2] = 1 + dp[ind1 - 1][ind2 - 1]; // match
+                    ans = max(ans, dp[ind1][ind2]);
+                } else {
+                    dp[ind1][ind2] = 0; // not match
+                }
+            }
+        }
+
+        return ans;
+    }
+};
